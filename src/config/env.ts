746af61511532cdb2e6fa394837ef1998388d6b1
Olivier Louvignes <olivier@mgcrea.io>
2023-01-30 18:08:54 +0100

    chore(config): bump default user agent

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,5 @@
-export const USER_AGENT = process.env.TYDOM_USER_AGENT || 'Tydom/255 CFNetwork/1125.2 Darwin/19.4.0';
+export const USER_AGENT =
+  process.env.TYDOM_USER_AGENT || 'DeltaDoreApplication-prod/686 CFNetwork/1404.0.5 Darwin/22.3.0';
 export const NODE_UNHANDLED_REJECTIONS = process.env.NODE_UNHANDLED_REJECTIONS;
 const NODE_MAJOR_VERSION = parseInt(process.versions.node.split('.')[0], 10);
 
