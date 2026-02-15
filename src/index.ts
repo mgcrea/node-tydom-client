4d4b9044e6b61955ee71695a5683b68f1dc9bc59
Olivier Louvignes <olivier@mgcrea.io>
2026-02-15 22:21:29 +0100

    test: update snapshots for new Client type
    Update test snapshots to reflect Client type change from Got function
    to plain { login } object.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import TydomClient, { createClient } from "./client";
 export type { TydomRequestBody } from "./client";
-export type { TydomBinaryMessage, TydomHttpMessage, TydomResponse } from "./utils/tydom";
 export type { MessageType } from "./utils/http";
+export type { TydomBinaryMessage, TydomHttpMessage, TydomResponse } from "./utils/tydom";
+export { createClient };
 
 export default TydomClient;
-export { createClient };
--- test/spec/utils/http.spec.ts
+++ test/spec/utils/http.spec.ts
@@ -1,19 +1,20 @@
-import { omit } from "lodash-es";
 import { parseIncomingMessage } from "src/utils/http";
 import { describe, expect, it } from "vitest";
 import { getInfoResponse, updatedDevicesData } from "../__fixtures__/messages";
 
 describe("parseIncomingMessage", () => {
   it("should properly parse a getInfoResponse response", async () => {
-    const message = await parseIncomingMessage(getInfoResponse);
-    expect(message).toBeDefined();
-    expect(Object.keys(message)).toMatchSnapshot();
-    expect(omit(message, "date")).toMatchSnapshot();
+    const result = await parseIncomingMessage(getInfoResponse);
+    expect(result).toBeDefined();
+    expect(Object.keys(result)).toMatchSnapshot();
+    const { date: _date1, ...message1 } = result as Record<string, unknown>;
+    expect(message1).toMatchSnapshot();
   });
   it("should properly parse a updatedDevicesData response", async () => {
-    const message = await parseIncomingMessage(updatedDevicesData);
-    expect(message).toBeDefined();
-    expect(Object.keys(message)).toMatchSnapshot();
-    expect(omit(message, "date")).toMatchSnapshot();
+    const result = await parseIncomingMessage(updatedDevicesData);
+    expect(result).toBeDefined();
+    expect(Object.keys(result)).toMatchSnapshot();
+    const { date: _date2, ...message2 } = result as Record<string, unknown>;
+    expect(message2).toMatchSnapshot();
   });
 });
