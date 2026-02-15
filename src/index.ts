12e2c4e1811c173695d22c4088fecd112917835c
Olivier Louvignes <olivier@mgcrea.io>
2026-02-15 21:08:40 +0100

    feat: export TypeScript types for public API
    Export TydomHttpMessage, TydomResponse, TydomBinaryMessage, and
    MessageType types to allow consumers to type their code properly.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import TydomClient, { createClient } from "./client";
+export type { TydomBinaryMessage, TydomHttpMessage, TydomResponse } from "./utils/tydom";
+export type { MessageType } from "./utils/http";
 
 export default TydomClient;
 export { createClient };
